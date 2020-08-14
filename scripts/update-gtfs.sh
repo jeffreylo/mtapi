8abcebb74ec375d660de1b34432d1aa81d5cfb98
Jeffrey Lo <hello@jeffreylo.cc>
2020-08-14 18:46:58 -0400

    build: update GTFS data

--- scripts/update-gtfs.sh
+++ scripts/update-gtfs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -eux
 
-basedir="${GOPATH}/src/github.com/jeffreylo/mtapi"
-mkdir ${basedir}/tmp
+basedir="$( cd "$( dirname "$0" )/.." && pwd )"
+rm -rf ${basedir}/tmp
+mkdir -p ${basedir}/tmp
 curl -sS http://web.mta.info/developers/data/nyct/subway/google_transit.zip > ${basedir}/tmp/mta_gtfs.zip
 unzip ${basedir}/tmp/mta_gtfs.zip -d ${basedir}/tmp
 rm ${basedir}/tmp/mta_gtfs.zip
-cp -a ${basedir}/tmp/. ${basedir}/data/gtfs
-rm -rf ${basedir}/tmp
+mkdir -p ${basedir}/mta/testdata/gtfs
+cp -a ${basedir}/tmp/. ${basedir}/mta/testdata/gtfs
